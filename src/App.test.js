754747937d810d843e275785d85db22276c6e2eb
reza razjoo <reza.razjoo@yahoo.com>
2020-06-23 20:39:46 +0100

    changed the index,js commented react.strictmode

--- src/App.test.js
+++ src/App.test.js
@@ -12,7 +12,7 @@ import ReactDOM from 'react-dom';
 
 it('renders without crashing', ()=> {
   const div = document.createElement('div');
-  ReactDOM.render(<app/>, div);
+  ReactDOM.render(<App/>, div);
 })
 
 
