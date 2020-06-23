d0f5cb6ab2bc1d6895f633c16b495b86203f05d8
reza razjoo <reza.razjoo@yahoo.com>
2020-06-23 21:56:55 +0100

    created continer and component and __tests__

--- src/components/__tests__/Header.test.js
+++ src/components/__tests__/Header.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import Header from './Header'
+import Header from '../Header'
 it('renders without crashing', ()=> {
     const div = document.createElement('div');
     ReactDom.render(<Header/>, div);
