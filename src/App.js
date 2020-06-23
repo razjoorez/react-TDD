31e97090e4f321d445d93f891a96c9899fa92f5e
reza razjoo <reza.razjoo@yahoo.com>
2020-06-23 21:48:21 +0100

    created header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +21,7 @@ function App() {
           Learn React
         </a>
         <p>Hello, Pluralsight</p>
-      </header>
+      </header> */}
     </div>
   );
 }
