2a63af9b3842063a8e6311f28d871b769ee830da
reza razjoo <reza.razjoo@yahoo.com>
2020-06-24 20:35:10 +0100

    installed enzyme and changed to shallow

--- src/__tests__/StoreLocator.test.js
+++ src/__tests__/StoreLocator.test.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import StoreLocator from '../StoreLocator';
 
 // describe("", function() {
    
 // })
 
-it('renders without carashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<StoreLocator />, div);
+// it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<StoreLocator />, div);
+// });
+
+it('renders without crashing', () => {
+    let mountedStoreLocator = shallow(<StoreLocator />);
 });
