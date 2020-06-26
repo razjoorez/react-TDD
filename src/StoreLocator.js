9a6d1b8d0cec4ce01834af54112daa69dddf9e9a
reza razjoo <reza.razjoo@yahoo.com>
2020-06-26 23:05:05 +0100

    Revert "created button and map components"
    This reverts commit ca1825380114b1f609ad1c03f684f570c2c23ad7.

--- src/StoreLocator.js
+++ src/StoreLocator.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
-import Header from '../components/Header';
-import Button from '../components/Button';
-import Map from '../components/Map';
 
 class StoreLocator extends Component {
     render() {
-        return (
-        <div>
-        <Header/>
-        <Button/>
-        <Button/>
-        <Map/>
-
-        </div>
-        
-        );
+        return ("");
     }
 }
 
--- src/__tests__/StoreLocator.test.js
+++ src/__tests__/StoreLocator.test.js
@@ -3,33 +3,15 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import StoreLocator from '../StoreLocator';
 
-describe("StoreLocator", function() {
+// describe("", function() {
    
-    let mountedStoreLocator ;
+// })
 
-beforeEach(() => {
-    mountedStoreLocator = shallow(<StoreLocator />);
-});
+// it('renders without crashing', () => {
+//     const div = document.createElement('div');
+//     ReactDOM.render(<StoreLocator />, div);
+// });
 
 it('renders without crashing', () => {
     let mountedStoreLocator = shallow(<StoreLocator />);
 });
-
-it('renders a header', () => {
-    const headers = mountedStoreLocator.find('Header');
-    expect(headers.length).toBe(1);
-});
-
-it('renders two buttons', () => {
-    const buttons = mountedStoreLocator.find('Button');
-    expect(buttons.length).toBe(2);
-});
-
-it('renders a map', () => {
-    const maps = mountedStoreLocator.find('Map');
-    expect(maps.length).toBe(1);
-})
-
-});
-
-
