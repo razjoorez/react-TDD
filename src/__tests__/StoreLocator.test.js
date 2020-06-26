36356fe813aaa3a36993d826573aad1e6ab77dbc
reza razjoo <reza.razjoo@yahoo.com>
2020-06-26 20:13:20 +0100

    Revert "installed enzyme and changed to shallow"
    This reverts commit 2a63af9b3842063a8e6311f28d871b769ee830da.

--- src/__tests__/StoreLocator.test.js
+++ src/__tests__/StoreLocator.test.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import StoreLocator from '../StoreLocator';
 
-describe("StoreLocator", function() {
+
    
-    let mountedStoreLocator ;
+    
 
 it('renders without carashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<StoreLocator />, div);
-}); 
 });
