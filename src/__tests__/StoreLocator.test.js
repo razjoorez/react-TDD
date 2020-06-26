6b9387080590d8052aba5857186f081b91b210fd
reza razjoo <reza.razjoo@yahoo.com>
2020-06-26 23:04:33 +0100

    Revert "created button and map components"
    This reverts commit ca1825380114b1f609ad1c03f684f570c2c23ad7.

--- src/__tests__/StoreLocator.test.js
+++ src/__tests__/StoreLocator.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import StoreLocator from '../StoreLocator';
 
+describe("StoreLocator", function() {
+   
+    let mountedStoreLocator ;
 
-it('renders without carashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<StoreLocator />, div);
+beforeEach(() => {
+    mountedStoreLocator = shallow(<StoreLocator />);
 });
+
+it('renders without crashing', () => {
+    let mountedStoreLocator = shallow(<StoreLocator />);
+});
+
+it('renders a header', () => {
+    const headers = mountedStoreLocator.find('Header');
+    expect(headers.length).toBe(1);
+});
+
+it('renders two buttons', () => {
+    const buttons = mountedStoreLocator.find('Button');
+    expect(buttons.length).toBe(2);
+});
+
+it('renders a map', () => {
+    const maps = mountedStoreLocator.find('Map');
+    expect(maps.length).toBe(1);
+})
+
+});
+
+
