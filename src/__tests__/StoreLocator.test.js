428d6e2435e5f53c28a2fce0a160722b1854bc0d
reza razjoo <reza.razjoo@yahoo.com>
2020-06-26 20:14:54 +0100

    removed describe from storlocater test

--- src/__tests__/StoreLocator.test.js
+++ src/__tests__/StoreLocator.test.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import StoreLocator from '../StoreLocator';
 
 
-   
-    
-
 it('renders without carashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<StoreLocator />, div);
