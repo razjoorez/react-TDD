ae774b8e6ebf1257fe9f6e19babe4b2d7838a9b7
reza razjoo <reza.razjoo@yahoo.com>
2020-06-26 01:42:10 +0100

    git reset ca1825380114b1f609ad1c03f684f570c2c23ad7

---
+++ src/__tests__/StoreLocator.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+import StoreLocator from '../StoreLocator';
+
+describe("StoreLocator", function() {
+   
+    let mountedStoreLocator ;
+
+beforeEach(() => {
+    mountedStoreLocator = shallow(<StoreLocator />);
+});
+
+it('renders without crashing', () => {
+    let mountedStoreLocator = shallow(<StoreLocator />);
+});
+
+it('renders a header', () => {
+    const headers = mountedStoreLocator.find('Header');
+    expect(headers.length).toBe(1);
+});
+
+it('renders two buttons', () => {
+    const buttons = mountedStoreLocator.find('Button');
+    expect(buttons.length).toBe(2);
+});
+
+it('renders a map', () => {
+    const maps = mountedStoreLocator.find('Map');
+    expect(maps.length).toBe(1);
+})
+
+});
+
+
