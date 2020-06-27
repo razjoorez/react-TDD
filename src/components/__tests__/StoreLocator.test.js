e76d63919da16e9b441f18da4027dd627edeb973
reza razjoo <reza.razjoo@yahoo.com>
2020-06-27 21:55:31 +0100

    shaoolw adaptor

--- src/components/__tests__/StoreLocator.test.js
+++ src/components/__tests__/StoreLocator.test.js
@@ -1,10 +1,17 @@
 import react, { Component } from 'react';
 import StoreLocator from '../../components/StoreLocator';
+import { Shallow, shallow } from 'enzyme';
 import ReactDOM from 'react-dom';
 
+//smoke test with jest
+// it("StoreLocator should render wothout crashing", ()=> {
+//     let div = document.createElement('div');
+//      ReactDOM.render('<StoreLocator/>', div);
+// })
 
+
+//Isolated test with Enzyme
 it("StoreLocator should render wothout crashing", ()=> {
-    let div = document.createElement('div');
-
-     ReactDOM.render('<StoreLocator/>', div);
+    let mountedStorLocator;
+    mountedStorLocator = shallow(<StoreLocator/>);
 })
