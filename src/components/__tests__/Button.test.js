80998b60d37e873d13c219a0d46f47f256b95fe0
System Administrator <reza.razjoo@yahoo.com>
2020-06-27 22:47:33 +0100

    tested for button tag

--- src/components/__tests__/Button.test.js
+++ src/components/__tests__/Button.test.js
@@ -3,7 +3,18 @@ import { Shallow , shallow} from 'enzyme';
 import Button from '../../components/Button';
 
 
-it("Should render without crashing", () => {
-    let mountedButton;
-    mountedButton = shallow(<Button/>);
+describe("Button", function(){
+
+    it("Should render without crashing", () => {
+        let mountedButton;
+        mountedButton = shallow(<Button/>);
+    });
+    it("Should have a button tag ", () => {
+        let mountedButton;
+        mountedButton = shallow(<Button/>);
+        let expected = mountedButton.find('button');
+        expect(expected.length).toBe(1);
+    });
+
 });
+
