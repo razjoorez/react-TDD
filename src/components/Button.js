5d0c9917866850af4b0fdada924ec03a0c187d11
reza razjoo <reza.razjoo@yahoo.com>
2020-06-28 01:41:27 +0100

    added text to button element

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,7 +3,7 @@ import React , { Component } from 'react';
 class Button extends Component {
 
     render() {
-        return (<button></button>);
+        return (<button>Button</button>);
     }
 }
 
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ import Header from '../components/Header';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
           <Header />
           <StoreLocator/>
           <Map/>
