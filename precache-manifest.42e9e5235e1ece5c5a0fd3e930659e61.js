2817b41e9b60fc74f52f19cc98b2b9529ef75247
razjoorez <reza.razjoo@yahoo.com>
2020-07-02 01:37:04 +0100

    Updates

---
+++ precache-manifest.42e9e5235e1ece5c5a0fd3e930659e61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf20124fbd49f9f0bc5450864a61345e",
+    "url": "/react-tdd/index.html"
+  },
+  {
+    "revision": "03ad80f091938bc69e08",
+    "url": "/react-tdd/static/css/main.0bd7d039.chunk.css"
+  },
+  {
+    "revision": "abd3d8ceb53000ce0fa8",
+    "url": "/react-tdd/static/js/2.447cc8b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tdd/static/js/2.447cc8b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03ad80f091938bc69e08",
+    "url": "/react-tdd/static/js/main.52edf6d4.chunk.js"
+  },
+  {
+    "revision": "c8639c9451537f34acc6",
+    "url": "/react-tdd/static/js/runtime-main.0d7cbb26.js"
+  }
+]);
